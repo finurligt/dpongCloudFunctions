e006ce5c5e74ddd8d9dfd416ecfef42f96c41a74
Fabian Sundholm <fabian.sundholm@hotmail.com>
2019-11-14 20:19:40 +0100

    it actually works tbh but losers elo change not implemented yet

--- functions/index.js
+++ functions/index.js
@@ -2,25 +2,70 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+//it might be better to do all this in a single transaction()
+//cuz something weird happens when i do it like this and the
+//transaction() of elo points fails a lot
+//(its ok tho firebase retries until it succeeds)
+//but im too lazy
 exports.submitGame = functions.https.onCall((data, context) => {
+  var gamesRef = admin.database().ref('/games');
+  var playerGamesRef = admin.database().ref('/playerGames');
+  var playersRef = admin.database().ref('/players');
 
-      var gamesRef = admin.database().ref('/games');
-      var playerGamesRef = admin.database().ref('/playerGames');
+  //calculating average team ratings
 
+  var winnersRating = 0;
+  var losersRating = 0;
+  playersRef.once("value").then(function(playersSnapshot) {
+    data.winners.forEach(function(entry) {
+      console.log(playersSnapshot.val());
+      console.log(entry);
+      console.log(playersSnapshot.val()[entry]);
+      winnersRating+=playersSnapshot.val()[entry].rating;
+    });
 
-      var gameData = {
-        winners: data.winners,
-        losers: data.losers,
-        timestamp: admin.database.ServerValue.TIMESTAMP
-      }
-      //return gamesRef.update({ Game: data.winner });
+    data.losers.forEach(function(entry) {
+      losersRating+=playersSnapshot.val()[entry].rating;
+    });
 
-      var newGame = gamesRef.push();
-      newGame.set(gameData);
+    winnersRating/=data.winners.length;
+    losersRating/=data.losers.length;
 
-      data.winners.concat(data.losers).forEach(function(entry) {
-        var game = playerGamesRef.child(entry).push(gameData);
-        game.set(gameData);
+    //insert elo math here
+    var expectedA = 1 / (1 + Math.pow(10,(losersRating-winnersRating)/400));
+
+    var kValue = 40;
+    var ratingChange = Math.round(kValue*(1-expectedA));
+
+    data.winners.forEach(function(entry) {
+      playersRef.child(entry).transaction(function(post) {
+        console.log(playersRef);
+        console.log(playersRef.child(entry));
+        console.log(entry);
+        console.log(post);
+        if (post!=null) {
+          post.rating+=ratingChange;
+        }
+        return post;
       });
+    });
+  });
 
+  var gameData = {
+    winners: data.winners,
+    losers: data.losers,
+    timestamp: admin.database.ServerValue.TIMESTAMP
+  }
+  //return gamesRef.update({ Game: data.winner });
+
+  var newGame = gamesRef.push();
+  newGame.set(gameData);
+
+
+
+  data.winners.concat(data.losers).forEach(function(entry) {
+    //add data to players "match histories"
+    var game = playerGamesRef.child(entry).push(gameData);
+    game.set(gameData);
+  });
 });
