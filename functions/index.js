84339be6d24c67e51fe4dca8407770083358e155
Fabian Sundholm <fabian.sundholm@hotmail.com>
2019-11-15 14:03:15 +0100

    actually working transaction

--- functions/index.js
+++ functions/index.js
@@ -8,42 +8,64 @@ exports.submitGame = functions.https.onCall((data, context) => {
   var playerGamesRef = admin.database().ref('/playerGames');
   var playersRef = admin.database().ref('/players');
 
+  //this wont generate any error is the log,
+  //only "Ignoring exception from a finished function"
+  //if u want logged errors:
+  //hardcode post variable or catch errors and log manually
   playersRef.transaction(function(post) {
+
     //calculating average team ratings
     if(post==null) {
       console.log("post is null, hopefully firebase will retry");
       return 0;
+    } else {
+      var winnersRating = 0;
+      var losersRating = 0;
+      data.winners.forEach(function(entry) {
+        winnersRating+=post[entry].rating;
+      });
+
+      data.losers.forEach(function(entry) {
+        losersRating+=post[entry].rating;
+      });
+
+      winnersRating/=data.winners.length;
+      losersRating/=data.losers.length;
+
+      //insert elo math here
+      var expectedA = 1 / (1 + Math.pow(10,(losersRating-winnersRating)/400));
+
+      var kValue = 40;
+      var ratingChange = Math.round(kValue*(1-expectedA));
+
+      data.winners.forEach(function(entry) {
+        post[entry].rating+=ratingChange;
+      });
+
+      data.losers.forEach(function(entry) {
+        post[entry].rating-=ratingChange;
+      });
+
+      //below is game logging
+      var gameData = {
+        winners: data.winners,
+        losers: data.losers,
+        timestamp: admin.database.ServerValue.TIMESTAMP,
+        rating: ratingChange
+      }
+
+      var newGame = gamesRef.push();
+      newGame.set(gameData);
+
+      data.winners.concat(data.losers).forEach(function(entry) {
+        //add data to players "match histories"
+        var game = playerGamesRef.child(entry).push(gameData);
+        game.set(gameData);
+      });
+
+      return post;
     }
-    var winnersRating = 0;
-    var losersRating = 0;
-    data.winners.forEach(function(entry) {
-      console.log(post);
-      console.log(entry);
-      console.log(post[entry]);
-      winnersRating+=entry[entry].rating;
-    });
 
-    data.losers.forEach(function(entry) {
-      losersRating+=playersSnapshot.val()[entry].rating;
-    });
-
-    winnersRating/=data.winners.length;
-    losersRating/=data.losers.length;
-
-    //insert elo math here
-    var expectedA = 1 / (1 + Math.pow(10,(losersRating-winnersRating)/400));
-
-    var kValue = 40;
-    var ratingChange = Math.round(kValue*(1-expectedA));
-
-    data.winners.forEach(function(entry) {
-      post[entry].rating+=ratingChange;
-    });
-
-    data.losers.forEach(function(entry) {
-      post[entry].rating-=ratingChange;
-    });
-    return post
   }, function(error, committed, snapshot) {
     if (error) {
         console.log("error in transaction");
@@ -52,26 +74,5 @@ exports.submitGame = functions.https.onCall((data, context) => {
     } else {
         console.log("Transaction Committed");
     }
-  }, true
-);
-
-  //below is game logging, not as important to get right
-  //so not done in transaction
-  var gameData = {
-    winners: data.winners,
-    losers: data.losers,
-    timestamp: admin.database.ServerValue.TIMESTAMP
-  }
-  //return gamesRef.update({ Game: data.winner });
-
-  var newGame = gamesRef.push();
-  newGame.set(gameData);
-
-
-
-  data.winners.concat(data.losers).forEach(function(entry) {
-    //add data to players "match histories"
-    var game = playerGamesRef.child(entry).push(gameData);
-    game.set(gameData);
-  });
+  }, true);
 });
