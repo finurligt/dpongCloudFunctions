8fd57c4ecc76da890d97da22f09dacc2257fe45d
Fabian Sundholm <fabian.sundholm@hotmail.com>
2019-11-30 02:13:46 +0100

    hopefully fixed submit bug and updated public

--- functions/index.js
+++ functions/index.js
@@ -15,25 +15,27 @@ exports.submitGame = functions.https.onCall((data, context) => {
     //hardcode post variable or catch errors and log manually
     var winnersPromiseArray = [];
     data.winners.forEach(function(entry) {
-      winnersPromiseArray.push(admin.database().ref('/players/'+entry))
+      winnersPromiseArray.push(admin.database().ref('/players/'+entry).once('value'));
     });
     var losersPromiseArray = [];
     data.losers.forEach(function(entry) {
-      losersPromiseArray.push(admin.database().ref('/players/'+entry))
+      losersPromiseArray.push(admin.database().ref('/players/'+entry).once('value'));
     });
     Promise.all([Promise.all(winnersPromiseArray),Promise.all(losersPromiseArray)]).then(function(arrayOfArray) {
       var winnersArray = arrayOfArray[0];
       var losersArray = arrayOfArray[1];
 
+      console.log(arrayOfArray);
+
       //calculating average team ratings
       var winnersRating = 0;
       var losersRating = 0;
       winnersArray.forEach(function(entry) {
-        winnersRating+=entry.rating;
+        winnersRating+=entry.val().rating;
       });
 
       losersArray.forEach(function(entry) {
-        losersRating+=entry.rating;
+        losersRating+=entry.val().rating;
       });
 
       winnersRating/=winnersArray.length;
@@ -46,48 +48,48 @@ exports.submitGame = functions.https.onCall((data, context) => {
       var ratingChange = Math.round(kValue*(1-expectedA));
 
       winnersArray.forEach(function(entry) {
-        admin.database().ref('/players/'+entry.name).transaction(function(post) {
+        admin.database().ref('/players/'+entry.val().name).transaction(function(post) {
           if (post==null) {
             return 0;
           } else {
-            return post.rating+=ratingChange;
+            post.rating+=ratingChange;
+            return post;
           }
         });
       });
 
       losersArray.forEach(function(entry) {
-        admin.database().ref('/players/'+entry.name).transaction(function(post) {
+        admin.database().ref('/players/'+entry.val().name).transaction(function(post) {
           if (post==null) {
             return 0;
           } else {
-            return post.rating-=ratingChange;
+            post.rating-=ratingChange;
+            return post;
           }
         });
       });
 
+      //below is game logging
+      var gameData = {
+        winners: data.winners,
+        losers: data.losers,
+        timestamp: admin.database.ServerValue.TIMESTAMP,
+        rating: ratingChange
+      }
 
+      var newGame = gamesRef.push();
+      newGame.set(gameData);
+
+      data.winners.concat(data.losers).forEach(function(entry) {
+        //add data to players "match histories"
+        var game = playerGamesRef.child(entry).push(gameData);
+        game.set(gameData);
+      });
 
       resolve({
         result: "success",
       });
     });
-  });
-
-  //below is game logging
-  var gameData = {
-    winners: data.winners,
-    losers: data.losers,
-    timestamp: admin.database.ServerValue.TIMESTAMP,
-    rating: ratingChange
-  }
-
-  var newGame = gamesRef.push();
-  newGame.set(gameData);
-
-  data.winners.concat(data.losers).forEach(function(entry) {
-    //add data to players "match histories"
-    var game = playerGamesRef.child(entry).push(gameData);
-    game.set(gameData);
   });
 
   //await promise;
--- public/profile.js
+++ public/profile.js
@@ -1,12 +1,8 @@
 var id;
 
 //get id
-var query = window.location.search.substring(1);
-var vars = query.split("&");
-for (var i=0;i<vars.length;i++) {
-  var pair = vars[i].split("=");
-  if(pair[0] == "id"){id = pair[1];}
-}
+var url = new URL(window.location.href)
+id = url.searchParams.get("id");
 
 firebase.database().ref('/players/').once('value').then(function(snapshot) {
   if (id=="") {
